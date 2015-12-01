bb27f406d9f9bd383518bd47b9a98fa062559571
edsilv <e.silverton@gmail.com>
2015-12-01 17:11:08 +0000

    added FB and GA tracking

--- scripts.js
+++ scripts.js
@@ -9,6 +9,11 @@ $(function() {
     var $uv = $('#uv');
     var $footer = $('footer');
 
+    var GACategories = {
+        interactions: "Interactions",
+        files: "Files"
+    };
+
     window.onresize = function() {
         resize();
     };
@@ -80,124 +85,19 @@ $(function() {
 
     function uvEventHandlers() {
 
-        $(document).bind('uv.onAuthorizationOccurred', function (event, obj) {
-            console.log('uv.onAuthorizationOccurred');
-        });
-
-        $(document).bind('uv.onCanvasIndexChangeFailed', function (event, obj) {
-            console.log('uv.onCanvasIndexChangeFailed');
-        });
-
-        $(document).bind('uv.onCanvasIndexChanged', function (event, obj) {
-            console.log('uv.onCanvasIndexChanged', obj);
-        });
-
-        $(document).bind('uv.onClickthroughOccurred', function (event, obj) {
-            console.log('uv.onClickthroughOccurred');
-        });
-
-        $(document).bind('uv.onCloseActiveDialogue', function (event, obj) {
-            console.log('uv.onCloseActiveDialogue');
-        });
-
-        $(document).bind('uv.onCloseLeftPanel', function (event, obj) {
-            console.log('uv.onCloseLeftPanel');
-        });
-
-        $(document).bind('uv.onCloseRightPanel', function (event, obj) {
-            console.log('uv.onCloseRightPanel');
-        });
-
-        $(document).bind('uv.onCreated', function (event, obj) {
-            console.log('uv.onCreated');
-            setTestIds();
-        });
-
-        $(document).bind('uv.onDownArrow', function (event, obj) {
-            console.log('uv.onDownArrow');
-        });
-
-        $(document).bind('uv.onDownload', function (event, obj) {
-            console.log('uv.onDownload', obj);
-        });
-
         $(document).bind('uv.onDrop', function (event, manifestUri) {
             console.log('uv.drop: ' + manifestUri);
             clearHashParams();
         });
 
-        $(document).bind('uv.onEnd', function (event, obj) {
-            console.log('uv.onEnd');
-        });
-
-        $(document).bind('uv.onEscape', function (event, obj) {
-            console.log('uv.onEscape');
-        });
-
-        $(document).bind('uv.onHideDownloadDialogue', function (event, obj) {
-            // uv uses onHideOverlay
-            console.log('uv.onHideDownloadDialogue');
-        });
-
-        $(document).bind('uv.onHideEmbedDialogue', function (event, obj) {
-            // uv uses onHideOverlay
-            console.log('uv.onHideEmbedDialogue');
-        });
-
-        $(document).bind('uv.onHideExternalContentDialogue', function (event, obj) {
-            console.log('uv.onHideExternalContentDialogue');
-        });
-
-        $(document).bind('uv.onHideGenericDialogue', function (event, obj) {
-            // uv uses onHideOverlay
-            console.log('uv.onHideGenericDialogue');
-        });
-
-        $(document).bind('uv.onHideInformation', function (event, obj) {
-            console.log('uv.onHideInformation');
-        });
-
-        $(document).bind('uv.onHideOverlay', function (event, obj) {
-            console.log('uv.onHideOverlay');
-        });
-
-        $(document).bind('uv.onHideSettingsDialogue', function (event, obj) {
-            // uv uses onHideOverlay
-            console.log('uv.onHideSettingsDialogue');
-        });
-
-        $(document).bind('uv.onHome', function (event, obj) {
-            console.log('uv.onHome');
-        });
-
-        $(document).bind('uv.onLeftArrow', function (event, obj) {
-            console.log('uv.onLeftArrow');
-        });
-
-        $(document).bind('uv.onLeftPanelCollapseFullFinish', function (event, obj) {
-            console.log('uv.onLeftPanelCollapseFullFinish');
-        });
-
-        $(document).bind('uv.onLeftPanelCollapseFullStart', function (event, obj) {
-            console.log('uv.onLeftPanelCollapseFullStart');
-        });
-
-        $(document).bind('uv.onLeftPanelExpandFullFinish', function (event, obj) {
-            console.log('uv.onLeftPanelExpandFullFinish');
-        });
-
-        $(document).bind('uv.onLeftPanelExpandFullStart', function (event, obj) {
-            console.log('uv.onLeftPanelExpandFullStart');
-        });
-
         $(document).bind('uv.onLoad', function (event, obj) {
-
-            console.log('uv.onLoad', obj);
-
-            closeEditor();
 
             bootstrapper = obj.bootstrapper;
             var locales = bootstrapper.config.localisation.locales;
+            var manifestUri = bootstrapper.params.manifestUri;
+
+            trackEvent(items, 'Viewed', manifestUri);
+            trackVariable(1, 'Viewing', manifestUri, 2);
 
             $('#locale').empty();
 
@@ -205,271 +105,135 @@ $(function() {
                 var l = locales[i];
                 $('#locale').append('<option value="' + l.name + '">' + l.label + '</option>');
             }
-
-            setSelectedLocale(bootstrapper.params.locale);
-
-            loadConfigSchema(function(schema) {
-                if (schema){
-                    createEditor(schema);
-                    if (!isIE8()) {
-                        $('#configEditor').show();
-                    }
-                }
-                $('footer').show();
-            });
         });
 
         $(document).bind('uv.onNotFound', function (event, obj) {
             console.log('uv.onNotFound');
         });
 
-        $(document).bind('uv.onOpenLeftPanel', function (event, obj) {
-            console.log('uv.onOpenLeftPanel');
-        });
-
-        $(document).bind('uv.onOpenExternalResource', function (event, obj) {
-            console.log('uv.onOpenExternalResource');
-        });
-
-        $(document).bind('uv.onOpenRightPanel', function (event, obj) {
-            console.log('uv.onOpenRightPanel');
-        });
-
-        $(document).bind('uv.onPageDown', function (event, obj) {
-            console.log('uv.onPageDown');
-        });
-
-        $(document).bind('uv.onPageUp', function (event, obj) {
-            console.log('uv.onPageUp');
-        });
-
-        $(document).bind('uv.onRedirect', function (event, obj) {
-            console.log('uv.onRedirect');
-        });
-
-        $(document).bind('uv.onRefresh', function (event, obj) {
-            console.log('uv.onRefresh');
-        });
-
-        $(document).bind('uv.onResourceDegraded', function (event, obj) {
-            console.log('uv.onResourceDegraded');
-        });
-
-        $(document).bind('uv.onReturn', function (event, obj) {
-            console.log('uv.onReturn');
-        });
-
-        $(document).bind('uv.onRightArrow', function (event, obj) {
-            console.log('uv.onRightArrow');
-        });
-
-        $(document).bind('uv.onRightPanelCollapseFullFinish', function (event, obj) {
-            console.log('uv.onRightPanelCollapseFullFinish');
-        });
-
-        $(document).bind('uv.onRightPanelCollapseFullStart', function (event, obj) {
-            console.log('uv.onRightPanelCollapseFullStart');
-        });
-
-        $(document).bind('uv.onRightPanelExpandFullFinish', function (event, obj) {
-            console.log('uv.onRightPanelExpandFullFinish');
-        });
-
-        $(document).bind('uv.onRightPanelExpandFullStart', function (event, obj) {
-            console.log('uv.onRightPanelExpandFullStart');
-        });
-
-        $(document).bind('uv.onSequenceIndexChanged', function (event, sequenceIndex) {
-            console.log('uv.onSequenceIndexChanged: ' + sequenceIndex);
-        });
-
-        $(document).bind('uv.onSettingsChanged', function (event, settings) {
-            console.log('uv.onSettingsChanged', settings);
-        });
-
-        $(document).bind('uv.onShowClickThroughDialogue', function (event, obj) {
-            console.log('uv.onShowClickThroughDialogue');
-        });
-
-        $(document).bind('uv.onShowDownloadDialogue', function (event, obj) {
-            console.log('uv.onShowDownloadDialogue');
-        });
-
-        $(document).bind('uv.onShowEmbedDialogue', function (event, obj) {
-            console.log('uv.onShowEmbedDialogue');
-        });
-
-        $(document).bind('uv.onShowExternalContentDialogue', function (event, obj) {
-            console.log('uv.onShowExternalContentDialogue');
-        });
-
-        $(document).bind('uv.onShowGenericDialogue', function (event, obj) {
-            console.log('uv.onShowGenericDialogue');
-        });
-
-        $(document).bind('uv.onShowHelpDialogue', function (event, obj) {
-            console.log('uv.onShowHelpDialogue');
-        });
-
-        $(document).bind('uv.onShowInformation', function (event, obj) {
-            console.log('uv.onShowInformation');
-        });
-
-        $(document).bind('uv.onShowLoginDialogue', function (event, obj) {
-            console.log('uv.onShowLoginDialogue');
-        });
-
-        $(document).bind('uv.onShowOverlay', function (event, obj) {
-            console.log('uv.onShowOverlay');
-        });
-
-        $(document).bind('uv.onShowSettingsDialogue', function (event, obj) {
-            console.log('uv.onShowSettingsDialogue');
-        });
-
-        $(document).bind('uv.onThumbSelected', function (event, obj) {
-            console.log('uv.onThumbSelected');
-        });
-
         $(document).bind('uv.onToggleFullScreen', function (event, obj) {
             isFullSreen = obj.isFullScreen;
         });
 
-        $(document).bind('uv.onUpArrow', function (event, obj) {
-            console.log('uv.onUpArrow');
+        $(document).bind('uv.onCreated', function (event, obj) {
+
         });
 
-        $(document).bind('uv.onUpdateSettings', function (event, obj) {
-            console.log('uv.onUpdateSettings');
+        $(document).bind('uv.onHideLoginDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Log in', 'Closed');
+        });
+
+        $(document).bind('uv.onHideEmbedDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Embed', 'Closed');
+        });
+
+        $(document).bind('uv.onHideDownloadDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Download', 'Closed');
+        });
+
+        $(document).bind('uv.onShowLoginDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Log in', 'Opened');
         });
 
         $(document).bind('uv.onViewFullTerms', function (event, obj) {
-            console.log('uv.onViewFullTerms');
+            trackEvent(GACategories.interactions, 'Ts & Cs', 'Viewed');
         });
 
-        $(document).bind('uv.onWindowUnload', function (event, obj) {
-            console.log('uv.onWindowUnload');
+        $(document).bind('uv.onExternalLinkClicked', function (event, url) {
+            if (url.indexOf('terms-and-conditions') != -1){
+                trackEvent(GACategories.interactions, 'Ts & Cs', 'Viewed');
+            }
         });
 
-        $(document).bind('seadragonExtension.onClearSearch', function (event, obj) {
-            console.log('seadragonExtension.onClearSearch');
+        $(document).bind('uv.onAcceptTerms', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Ts & Cs', 'Accepted');
         });
 
-        $(document).bind('seadragonExtension.onCurrentViewUri', function (event, obj) {
-            console.log('seadragonExtension.onCurrentViewUri');
+        $(document).bind('uv.onShowDownloadDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Download', 'Opened');
         });
 
-        $(document).bind('seadragonExtension.onFirst', function (event, obj) {
-            console.log('seadragonExtension.onFirst');
+        $(document).bind('seadragonExtension.onDownloadCurrentView', function (event, obj) {
+            trackEvent(GACategories.files, 'Downloaded - Current View');
         });
 
-        $(document).bind('seadragonExtension.onGalleryThumbSelected', function (event, obj) {
-            console.log('seadragonExtension.onGalleryThumbSelected');
+        $(document).bind('seadragonExtension.onDownloadWholeImageHighRes', function (event, obj) {
+            trackEvent(GACategories.files, 'Downloaded - Whole Image High Res');
         });
 
-        $(document).bind('seadragonExtension.onImageSearch', function (event, obj) {
-            console.log('seadragonExtension.onImageSearch');
+        $(document).bind('seadragonExtension.onDownloadWholeImageLowRes', function (event, obj) {
+            trackEvent(GACategories.files, 'Downloaded - Whole Image Low Res');
         });
 
-        $(document).bind('seadragonExtension.onLast', function (event, obj) {
-            console.log('seadragonExtension.onLast');
+        $(document).bind('seadragonExtension.onDownloadEntireDocumentAsPDF', function (event, obj) {
+            trackEvent(GACategories.files, 'Downloaded - Entire Document As PDF');
         });
 
-        $(document).bind('seadragonExtension.onModeChanged', function (event, obj) {
-            console.log('seadragonExtension.onModeChanged', obj);
+        $(document).bind('seadragonExtension.onDownloadEntireDocumentAsText', function (event, obj) {
+            trackEvent(GACategories.files, 'Downloaded - Entire Document As Text');
         });
 
-        $(document).bind('seadragonExtension.onNext', function (event, obj) {
-            console.log('seadragonExtension.onNext');
+        $(document).bind('uv.onShowEmbedDialogue', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Embed', 'Opened');
         });
 
-        $(document).bind('seadragonExtension.onNextSearchResult', function (event, obj) {
-            console.log('seadragonExtension.onNextSearchResult');
+        $(document).bind('uv.onToggleFullScreen', function (event, obj) {
+            if (obj.isFullScreen) {
+                trackEvent(GACategories.interactions, 'Full Screen', 'Enter');
+            } else {
+                trackEvent(GACategories.interactions, 'Full Screen', 'Exit');
+            }
         });
 
-        $(document).bind('seadragonExtension.onOpenThumbsView', function (event, obj) {
-            console.log('seadragonExtension.onOpenThumbsView');
+        $(document).bind('uv.onOpenLeftPanel', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Left Panel', 'Opened');
+        });
+
+        $(document).bind('uv.onCloseLeftPanel', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Left Panel', 'Closed');
         });
 
         $(document).bind('seadragonExtension.onOpenTreeView', function (event, obj) {
-            console.log('seadragonExtension.onOpenTreeView');
+            trackEvent(GACategories.interactions, 'Tree', 'Opened');
         });
 
-        $(document).bind('seadragonExtension.onPageSearch', function (event, obj) {
-            console.log('seadragonExtension.onPageSearch');
+        $(document).bind('seadragonExtension.onOpenThumbsView', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Thumbs', 'Opened');
         });
 
-        $(document).bind('seadragonExtension.onPrev', function (event, obj) {
-            console.log('seadragonExtension.onPrev');
+        $(document).bind('uv.onOpenRightPanel', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Right Panel', 'Opened');
         });
 
-        $(document).bind('seadragonExtension.onPrevSearchResult', function (event, obj) {
-            console.log('seadragonExtension.onPrevSearchResult');
-        });
-
-        $(document).bind('seadragonExtension.onAnimation', function (event, obj) {
-            console.log('seadragonExtension.onAnimation');
-        });
-
-        $(document).bind('seadragonExtension.onAnimationfinish', function (event, obj) {
-            console.log('seadragonExtension.onAnimationfinish');
-        });
-
-        $(document).bind('seadragonExtension.onAnimationStart', function (event, obj) {
-            console.log('seadragonExtension.onAnimationStart');
-        });
-
-        $(document).bind('seadragonExtension.onOpen', function (event, obj) {
-            console.log('seadragonExtension.onOpen');
-        });
-
-        $(document).bind('seadragonExtension.onSearchPreviewStart', function (event, obj) {
-            console.log('seadragonExtension.onSearchPreviewStart');
-        });
-
-        $(document).bind('seadragonExtension.onSearchPreviewFinish', function (event, obj) {
-            console.log('seadragonExtension.onSearchPreviewFinish');
-        });
-
-        $(document).bind('seadragonExtension.onRotation', function (event, obj) {
-            console.log('seadragonExtension.onRotation');
-        });
-
-        $(document).bind('seadragonExtension.onSearch', function (event, obj) {
-            console.log('seadragonExtension.onSearch', obj);
-        });
-
-        $(document).bind('seadragonExtension.onSearchResults', function (event, obj) {
-            console.log('seadragonExtension.onSearchResults', obj);
-        });
-
-        $(document).bind('seadragonExtension.onSearchResultsEmpty', function (event, obj) {
-            console.log('seadragonExtension.onSearchResultsEmpty');
-        });
-
-        $(document).bind('seadragonExtension.onTreeNodeSelected', function (event, obj) {
-            console.log('seadragonExtension.onTreeNodeSelected', obj);
-        });
-
-        $(document).bind('seadragonExtension.onViewPage', function (event, obj) {
-            console.log('seadragonExtension.onViewPage', obj);
-        });
-
-        $(document).bind('seadragonExtension.onCurrentViewUri', function (event, obj) {
-            console.log('seadragonExtension.onCurrentViewUri', obj);
-        });
-
-        $(document).bind('mediaelementExtension.onMediaEnded', function (event, obj) {
-            console.log('mediaelementExtension.onMediaEnded');
-        });
-
-        $(document).bind('mediaelementExtension.onMediaPaused', function (event, obj) {
-            console.log('mediaelementExtension.onMediaPaused');
+        $(document).bind('uv.onCloseRightPanel', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Right Panel', 'Closed');
         });
 
         $(document).bind('mediaelementExtension.onMediaPlayed', function (event, obj) {
-            console.log('mediaelementExtension.onMediaPlayed');
+            trackEvent(GACategories.interactions, 'Play');
         });
+
+        $(document).bind('mediaelementExtension.onMediaPaused', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Pause');
+        });
+
+        $(document).bind('mediaelementExtension.onMediaEnded', function (event, obj) {
+            trackEvent(GACategories.interactions, 'Ended');
+        });
+    }
+
+    function trackEvent(category, action, label) {
+        _gaq.push(['_trackEvent', category, action, label]);
+    }
+
+    /**
+     * @param {number} slot - 1-5 (5 slots per scope)
+     * @param {string} name - the name for the custom variable
+     * @param {number} value - the value of the custom variable
+     * @param {string} scope - visitor, session, page
+     */
+    function trackVariable(slot, name, value, scope) {
+        _gaq.push(['_setCustomVar', Number(slot), name, value, Number(scope)]);
     }
 
     function init() {
@@ -516,6 +280,10 @@ $(function() {
         $manifest.on('dragover', function(e) {
             // allow drop
             e.preventDefault();
+        });
+
+        $manifest.click(function() {
+            $(this).select();
         });
 
         uvEventHandlers();
