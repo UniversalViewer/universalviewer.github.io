74a8fd2d22814097a684b648c72024c7994500c4
Demian Katz <demian.katz@villanova.edu>
2026-02-11 09:35:46 -0500

    Remove Vercel plugin (not needed for static site).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
-export default defineConfig({
-  adapter: vercel()
-});
+export default defineConfig();
