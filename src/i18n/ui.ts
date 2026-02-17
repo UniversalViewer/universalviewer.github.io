f0c912947ab55ef1db52bfd0a3a5329b9e7c05c2
LlGC-jop <jop@llgc.org.uk>
2026-02-17 13:54:20 +0000

    starter ui lang file

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  cy: 'Cymraeg',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.demo': 'Demo',
+  },
+  cy: {
+    'nav.home': 'Hafan',
+    'nav.about': 'Am',
+    'nav.demo': 'Dangos',
+  },
+} as const;
