dbe537b8971661705ba43dd74a4244901c0d14ce
LlGC-jop <jop@llgc.org.uk>
2026-02-17 14:14:24 +0000

    add more lang strings

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,12 +8,18 @@ export const defaultLang = 'en';
 export const ui = {
   en: {
     'nav.home': 'Home',
-    'nav.about': 'About',
     'nav.demo': 'Demo',
+    'nav.events': 'Events',
+    'nav.documentation': 'Documentation',
+    'nav.blog': 'Blog',
+    'nav.community': 'Community',
   },
   cy: {
     'nav.home': 'Hafan',
-    'nav.about': 'Am',
-    'nav.demo': 'Dangos',
+    'nav.demo': 'Arddangos',
+    'nav.events': 'Digwyddiadau',
+    'nav.documentation': 'Dogfennaeth',
+    'nav.blog': 'Blog',
+    'nav.community': 'Cymuned',
   },
 } as const;
