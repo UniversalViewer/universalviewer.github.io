f52d08c9909403be6189ce106ef9c493601023d9
LlGC-jop <jop@llgc.org.uk>
2026-02-25 12:10:41 +0000

    localise homepage uv

---
+++ src/config/uv.ts
@@ -0,0 +1,4 @@
+export const uvLocales = {
+  en: 'en-GB:English+(GB),cy-GB:Cymraeg,fr-FR:Français+(FR),pl-PL:Polski,sv-SE:Svenska',
+  cy: 'cy-GB:Cymraeg,en-GB:English+(GB),fr-FR:Français+(FR),pl-PL:Polski,sv-SE:Svenska',
+};
